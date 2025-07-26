18e7bd7206378cc7bfb04d72f70de29adeeed79d
mizuwonomu <sonn3061@gmail.com>
2025-07-26 16:07:06 +0700

    check vector stores and print chunks

--- main.py
+++ main.py
@@ -1,25 +1,28 @@
 from src.data_processing import doc_cleaning, chunking_doc
-
-file_path = "data\\summary.txt"
+from src.vector_store import store_embeddings
+file_path = "data/summary.txt"
 
 def main():
     cleaned_text = doc_cleaning(file_path)
-
-    print("\n---Sau khi cleaning (300 kí tự):")
-    print(cleaned_text[:300])
-    print("-----------------\n")
-
+    
     text_chunks = chunking_doc(cleaned_text)
 
-    print("\n--Hai chunk đầu tiên----")
-    if (len(text_chunks) > 0):
-        print("---Chunk 1---")
-        print((text_chunks[0]))
-    if (len(text_chunks) > 1):
-        print("---Chunk 2---")
-        print((text_chunks[1]))
+    vector_store = store_embeddings(text_chunks)
 
-    print("--------------------")
+    print("\nToàn bộ quá trình đã xong")
+
+    print("\nChecking vector store:...")
+
+    query = "Kế hoạch A và B của NASA là gì"
+
+    results = vector_store.similarity_search(query)
+    print(f"\nTìm thấy {len(results)} chunks liên quan đến {query}")
+    print("---Nội dung chunk đầu tiên tìm được---")
+
+    if(results):
+        print(results[0].page_content)
+    else:
+        print("Không tìm thấy chunk nào phù hợp")
 
 if __name__ == "__main__":
     main()
