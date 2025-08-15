5d226c8309ed5e0463830759a4df05a9be34ee62
mizuwonomu <sonn3061@gmail.com>
2025-08-15 14:11:07 +0700

    Added citing sources via metadata

--- src/data_processing.py
+++ src/data_processing.py
@@ -21,15 +21,39 @@ def doc_cleaning(file_path):
     #bất cứ đoạn nào có nhiều hơn enter 3 lần liên tiếp, sẽ chuyển về định dạng ngắt dòng chuẩn là \n\n
     return cleaned_text
 
-def chunking_doc(text):
+def chunking_doc(text, file_path = None, additional_metadata= None, chunk_size = 500, chunk_overlap = 50):
     text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size = 500,
-        chunk_overlap = 50, #dùng để  "nhắc lại các số lượng kí tự cuối". ví dụ như chunk 1 có 50 kí tự cuối, thì mở đầu chunk 2 cx sẽ có bấy nhiêu kí tự từ chunk 1
+        chunk_size = chunk_size,
+        chunk_overlap = chunk_overlap, #dùng để  "nhắc lại các số lượng kí tự cuối". ví dụ như chunk 1 có 50 kí tự cuối, thì mở đầu chunk 2 cx sẽ có bấy nhiêu kí tự từ chunk 1
         length_function = len, #sử dụng chính hàm len để đo độ dài chuỗi, đánh dấu toàn bộ như dấu cách, kí tự,..
     )
+    from datetime import datetime
+    base_metadata = { #mẫu chung để định danh cho các chunks
+        "source": file_path or "unknown",
+        "source_type": "txt",
+        "processed_date": datetime.now().isoformat(),
+        "original_length": len(text),
+        "chunk_size": chunk_size,
+        "chunk_overlap": chunk_overlap,
+        "version": "1.0" 
+    #Đây là các dictionary 
+    }
 
-    docs_to_split = [Document(page_content = text)] #Convert string sang object, 1 list chứa 1 document
+    if additional_metadata:
+        base_metadata.update(additional_metadata) #nếu như có bất cứ thông tin định dạng nào cho vào, update thêm
+
+    docs_to_split = [Document(page_content = text, metadata = base_metadata)] #Convert string sang object, 1 list chứa 1 document
     chunks = text_splitter.split_documents(docs_to_split) #nhận vào list document và trả về list document
+    
+    for i,chunk in enumerate(chunks):
+        chunk.metadata.update({
+            "chunk_id": i, 
+            "chunk_index": f"{(i+1)/len(chunks)}", #chỉ định vị trí index thứ mấy trong tổng chunk, ví dụ như chunk 3/8
+            "chunk_size": len(chunk.page_content), #độ dài thực tế của chunk 
+            "chunk_start_char": i * max(0,chunk_size - chunk_overlap) #"highlight" đúng đoạn văn bản gốc trên giao diện khi trích dẫn nguồn
+        })
+    for chunk in chunks:
+        chunk.metadata["total_chunks"] = len(chunks)
     print(f"Cắt thành công {len(chunks)} chunks.")
 
     return chunks
--- src/qa_chain.py
+++ src/qa_chain.py
@@ -2,26 +2,25 @@ import os
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_google_genai import GoogleGenerativeAIEmbeddings, GoogleGenerativeAI
 from langchain_chroma import Chroma
-from langchain.prompts import PromptTemplate
-from langchain.schema.runnable import RunnablePassthrough
+from langchain.schema.runnable import RunnablePassthrough #chạy nhiều nhánh xử lý cùng 1 lúc
 from langchain.schema.output_parser import StrOutputParser
+from langchain.schema.runnable import RunnableParallel
 from dotenv import load_dotenv
 
 load_dotenv()
 CHROMA_PATH = "chroma_db"
 
-def get_chain():
+def get_chain(k = 3, temperature = 0.4):
     embedding_model = GoogleGenerativeAIEmbeddings(model = "models/gemini-embedding-001")
     #load vector store
     vector_store = Chroma(
         embedding_function = embedding_model,
         persist_directory = CHROMA_PATH
     )
-
     #Generate retriever
-    retriever = vector_store.as_retriever(search_kwargs = {'k': 3}) #tìm 3 chunks liên quan nhất
+    retriever = vector_store.as_retriever(search_kwargs = {'k': k}) #tìm 3 chunks liên quan nhất
 
-    llm = GoogleGenerativeAI(model = "models/gemini-2.5-flash", temperature = 0.4)
+    llm = GoogleGenerativeAI(model = "models/gemini-2.5-flash", temperature = temperature)
 
     template = """
     Dựa vào những thông tin dưới đây để trả lời câu hỏi. Nếu không thể tìm thấy câu trả lời cho câu hỏi, hãy trả lời 'Tôi không thể tìm thấy
@@ -29,19 +28,22 @@ def get_chain():
     Context:
     {context}
     Question:
-    {input}
+    {question}
 
     Answer:
     """
     prompt = ChatPromptTemplate.from_template(template)
-
-    rag_chain = (
-        {"context":retriever, "input": RunnablePassthrough()}
-        | prompt
+    answer_chain = (
+        prompt
         | llm
-        | StrOutputParser() #lấy kết quả từ gemini và chuyển thành text, chọn string có top result cao nhất
+        | StrOutputParser()
+    )
+    rag_chain=RunnableParallel(
+            context = retriever,
+            question = RunnablePassthrough(),
+    ).assign(
+        #answer được chạy qua 1 chain con
+        answer = answer_chain
     )
 
     return rag_chain
-
-
