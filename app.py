993b3f3b17388a619b70083620ac95c8af2dd68c
mizuwonomu <sonn3061@gmail.com>
2026-02-06 22:06:59 +0700

    feat(UI): Implement real-time response streaming - Integrated Langchain's streaming tokens with Streamlit's st.write_stream. - Improved perceived latency for RAG responses - Handled callback handlers for smooth token delivery. feat(UI):ストリーミング応答機能の実装 LangChainのトーケンストリーミングとStreamlitのst.write_streamを連携。 RAGの体感遅延を改善 コールバックハンドラによるスムーズなトーケン配信を制御

--- app.py
+++ app.py
@@ -44,6 +44,28 @@ with st.sidebar:
 k_value = k_slider
 temperature_value = temperature_slider
 
+def stream_handler(chain, question, session_id):
+
+    #input(入力) phải là Dict (辞書型), gồm answer, context, nên ta phải tách answer cho Streamlit hiển thị
+    stream = chain.stream(
+        {"question": question},
+        config={"configurable": {"session_id": session_id}}
+    )
+
+    #Biến（変数) dùng để lưu lại nguồn (参照元の保存) (do nguồn thường trả về 1 cục, không stream từng chữ) 
+    full_context = None
+
+    for chunk in stream:
+        if "context" in chunk:
+            full_context = chunk["context"]
+
+        if "answer" in chunk:
+            yield chunk["answer"] #stream từng chữ cho streamlit
+
+    st.session_state.last_context = full_context
+    
+
+
 def load_chain(k,temperature):
     st.write(f"--Đang trả về chain với k ={k} và temperature = {temperature}")
     return get_chain(k = k, temperature = temperature)
@@ -51,26 +73,25 @@ def load_chain(k,temperature):
 rag_chain = load_chain(k = k_value, temperature = temperature_value)
 question = st.text_input("Nhập câu hỏi của m đi con")
 
+if "last_context" not in st.session_state:
+    st.session_state.last_context = []
+
 if question:
-    with st.spinner("Đang nghĩ..."):
+    with st.chat_message("user"):
+        st.write(question)
+
+    with st.chat_message("ai"):
+
         session_id = "user_vjp_pro_1"
 
-        response = rag_chain.invoke(
-            {"question": question},
-            config={"configurable": {"session_id": session_id}}
-        )
-        answer = response['answer']
-        source_documents = response['context']
+        #dùng st.write_stream để nhận generator 'yield' ở trên
+        full_response = st.write_stream(stream_handler(rag_chain, question, session_id))
 
-        st.write("##Câu trả lời:")
-        st.write(answer)
+        if st.session_state.last_context:
+            with st.expander("📚 Nguồn tài liệu (Context)"):
+                st.write(st.session_state.last_context)
 
-        if source_documents:
-            st.write("##Các nguồn sử dụng:")
-            for i,doc in enumerate(source_documents):
-                source = doc.metadata.get("source", "Không rõ nguồn")
-                with st.expander(f"Nguồn {i+1}: {source}"):
-                    st.write(doc.page_content)
+
 
 
         
