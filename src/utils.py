46fa7052efd005971ce478d1ce234fd8ae4396b6
mizuwonomu <sonn3061@gmail.com>
2026-02-11 15:56:00 +0000

    refactor(embedding): Switch embedding backend from API to local model bge-m3 > refactor(埋め込み):埋め込みモデルAPIベースからローカルのbge-m3モデルに切り替え

---
+++ src/utils.py
@@ -0,0 +1,24 @@
+import torch
+from langchain_huggingface import HuggingFaceEmbeddings
+
+def get_device():
+    if torch.cuda.is_available():
+        return "cuda"
+
+def get_embedding_model():
+    device = get_device()
+    print(f"Đang load embedding model 'BAAI/bg3-m3'.... {device.upper()}")
+
+    model_name = "BAAI/bge-m3"
+
+    model_kwargs = {'device': device}
+    encode_kwargs = {'normalize_embeddings': True}
+
+    embedding_model = HuggingFaceEmbeddings(
+        model_name=model_name,
+        model_kwargs=model_kwargs,
+        encode_kwargs=encode_kwargs
+    )
+
+    return embedding_model
+    
